9ed31e0bc4af2823325db5ed8a4457235af55aba
enewhall <ewnewhal@gmail.com>
2014-11-23 16:01:00 -0500

    Lab 13 Sunday 4pm

--- Lab13/Lab13.m
+++ Lab13/Lab13.m
@@ -3,44 +3,59 @@ function Lab13(robot)
     global RobotEstimate ;
     
     thePose = pose(.23,.23,-pi/2);
+    
     RobotEstimate = estRobot(thePose.x,thePose.y,thePose.th,robot);
     
     LineMap.makeMap();
     robot.forksDown();
-    backUp(robot,false);
-    %getNewPose(robot);
-    pickUpY =1.40;
-    dropOffY = .30;
+    
+    robot.sendVelocity(-.1,.1);
+    pause(1.8);
+    robot.sendVelocity(.2,.2);
+    pause(3);
+    robot.sendVelocity(.2,.2);
+    pause(1);
+    
+    getNewPose(robot);
+    
+    pickUpY =1.35;
+    dropOffY = .28;
+    
+    %object 10 (side) 
+    pickUpPose = pose(1.9,.6,0);
+    dropOffPose = pose(.30,dropOffY,-pi/2);
+    getSideObject(pickUpPose, dropOffPose, robot);
+    
     
     %object 1
     pickUpPose = pose(.30,pickUpY,pi/2);
-    dropOffPose = pose(.30,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*2,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 2
     pickUpPose = pose(.30*2,pickUpY,pi/2);
-    dropOffPose = pose(.30*2,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*3,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 3
     pickUpPose = pose(.30*3,pickUpY,pi/2);
-    dropOffPose = pose(.30*3,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*4,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 4
     pickUpPose = pose(.30*4,pickUpY,pi/2);
-    dropOffPose = pose(.30*4,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*5,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 5 
     pickUpPose = pose(.30*5,pickUpY,pi/2);
-    dropOffPose = pose(.30*5,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*6,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 6 
     pickUpPose = pose(.30*6,pickUpY,pi/2);
-    dropOffPose = pose(.30*6,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,false);
+    dropOffPose = pose(.30*7,dropOffY,-pi/2);
+    getObject(pickUpPose,dropOffPose,robot);
     
     %object 7 (dont attempt)
     %pickUpPose = pose(.30*7,pickUpY,pi/2);
@@ -50,16 +65,13 @@ function Lab13(robot)
     %object 8 (side) (dont attempt)
     %object 9 (side) (dont attempt)
     
-    %object 10 (side) 
-    pickUpPose = pose(.3*7,pickUpY,pi/2);
-    dropOffPose = pose(.30*7,dropOffY,-pi/2);
-    getObject(pickUpPose,dropOffPose,robot,true);
+    
     
     robot.sendVelocity(0,0);    
 end
 
 
-function getObject(pickUpPose,dropOffPose,robot,avoid)
+function getObject(pickUpPose,dropOffPose,robot)
     global thePose;
     pauseTime = .01;
     
@@ -78,7 +90,7 @@ function getObject(pickUpPose,dropOffPose,robot,avoid)
     executeTrajectory(Ttr(1,3),Ttr(2,3),atan2(Ttr(2,1),Ttr(2,2)),robot,pauseTime); %get close to target
     goToTarget(robot); % scan and go to pickup pose
     pickUp(robot);
-    backUp(robot,avoid);
+    backUp(robot);
     getNewPose(robot);
     Ttw = [cos(thf1d) -sin(thf1d) xf1d ; sin(thf1d) cos(thf1d) yf1d ; 0 0 1];
     Trw = [cos(thePose.th) -sin(thePose.th) thePose.x; sin(thePose.th) cos(thePose.th) thePose.y; 0 0 1];
@@ -87,6 +99,42 @@ function getObject(pickUpPose,dropOffPose,robot,avoid)
     dropOff(robot);
     reverse(robot);
 
+end
+
+function getSideObject(pickUpPose,dropOffPose,robot)
+    global thePose;
+    pauseTime = .01;
+    
+    xf1p = pickUpPose.x;
+    yf1p = pickUpPose.y;
+    thf1p = pickUpPose.th;
+    
+    xf1d = dropOffPose.x;
+    yf1d = dropOffPose.y;
+    thf1d = dropOffPose.th;
+    
+    getNewPose(robot);
+    Ttw = [cos(thf1p) -sin(thf1p) xf1p ; sin(thf1p) cos(thf1p) yf1p ; 0 0 1];
+    Trw = [cos(thePose.th) -sin(thePose.th) thePose.x; sin(thePose.th) cos(thePose.th) thePose.y; 0 0 1];
+    Ttr = inv(Trw) * Ttw;
+    executeTrajectory(Ttr(1,3),Ttr(2,3),atan2(Ttr(2,1),Ttr(2,2)),robot,pauseTime); %get close to target
+    
+    robot.sendVelocity(.15,.15);
+    pause(.8);
+    pickUp(robot);
+    backUp(robot);
+    robot.sendVelocity(.2,.2);
+    pause(4);
+    robot.sendVelocity(.2,.2);
+    pause(4);
+    getNewPose(robot);
+    Ttw = [cos(thf1d) -sin(thf1d) xf1d ; sin(thf1d) cos(thf1d) yf1d ; 0 0 1];
+    Trw = [cos(thePose.th) -sin(thePose.th) thePose.x; sin(thePose.th) cos(thePose.th) thePose.y; 0 0 1];
+    Ttr = inv(Trw) * Ttw;
+    executeTrajectory(Ttr(1,3)-.18,Ttr(2,3),atan2(Ttr(2,1),Ttr(2,2)),robot,pauseTime); %get close to target
+    dropOff(robot);
+    reverse(robot);
+    
 end
 
 
@@ -125,7 +173,7 @@ function goToTarget(robot)
     x = image.xArray(bestIndex);
     y = image.yArray(bestIndex);
 
-
+    th=-pi/2;
     % convert to robot coordinates     
     %object in sensor coordinates
     Tos = [cos(th) -sin(th) x ; sin(th) cos(th) y ; 0 0 1];
@@ -148,11 +196,12 @@ function goToTarget(robot)
 %     if(abs(th > pi/6))
 %         pause(1);
 %     end
-    executeTrajectory(x,y,th,robot,1);
+    executeTrajectory(x,y,th,robot,.1);
     robot.sendVelocity(.1,.1);
     pause(1);
     robot.sendVelocity(0,0);
 end
+
 
 function reverse(robot)
     robot.sendVelocity(-.2,-.2);
@@ -168,21 +217,15 @@ function reverse(robot)
     robot.sendVelocity(0,0);
 end
 
-function backUp(robot, avoid)
+function backUp(robot)
 
     robot.sendVelocity(-.1,.1);
-    if avoid == false
-        pause(.2);
-        robot.sendVelocity(-.1,.1);
-    end
+    pause(.2);
+    robot.sendVelocity(-.1,.1);
     pause(4);
-    if avoid == false
-        robot.sendVelocity(.2,.2);
-        pause(3);
-    else
-        robot.sendVelocity(.10,.2);
-        pause(3);
-    end
+    
+    robot.sendVelocity(.2,.2);
+    pause(2);
     robot.sendVelocity(0,0);
 end
 
